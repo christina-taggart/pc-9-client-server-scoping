70685909bbe75370671938a42a9c0c14fa0e61a6
Christina Taggart <ctaggart@Christinas-MacBook-Pro-2.local>
2014-02-09 17:46:27 -0800

    Modify skills page so button updates heading with the skill in the URL

--- source/client-server-scoping/public/js/application.js
+++ source/client-server-scoping/public/js/application.js
@@ -3,5 +3,13 @@ $(document).ready(function() {
     $('#awesomeness_holder').html( $(this).data('heading') )
   });
 
+  $('.skill_teller').on('click', function(){
+    console.log(this)
+    var url = document.URL;
+    var splitURL = url.split('/');
+    var skill = splitURL[splitURL.length - 1];
+    $('#skill_holder').html(skill)
+
+  })
   // What other events do you need to bind to make the other pages work?
 });
