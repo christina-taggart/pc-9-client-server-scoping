2fc649a74b0c0b4b5590297b6165c9a080408351
Christina Taggart <ctaggart@Christinas-MacBook-Pro-2.local>
2014-02-09 17:27:19 -0800

    Modify index page so buttons update heading

--- source/client-server-scoping/public/js/application.js
+++ source/client-server-scoping/public/js/application.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $('.awesomeness_teller').on('click', function() {
-
+    $('#awesomeness_holder').html( $(this).data('heading') )
   });
 
   // What other events do you need to bind to make the other pages work?
